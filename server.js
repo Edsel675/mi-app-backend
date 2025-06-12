8719beab97a1714caa735f0e89090dc518c1aeaf
Edsel De Jesus Cisneros Bautista <111032109+Edsel675@users.noreply.github.com>
2025-06-11 22:54:12 -0600

    Mejoro control de fetch y validación en formulario

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://edsel675.github.io', // Permitir solo tu frontend en GitHub Pages
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 
 // Rutas
